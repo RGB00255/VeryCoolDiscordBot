efd6a4584f05e5f0fb5658030f0582e185759d35
Ryan Bergeron <ryan.bergeron1998@gmail.com>
2020-05-08 16:00:04 -0400

    Added listsounds command

--- sound.py
+++ sound.py
@@ -1,6 +1,6 @@
 # sound.py handles everything related to sound
 from discord.ext import commands
-import discord
+import discord, os
 
 # Sound commands
 class Sound(commands.Cog):
@@ -16,6 +16,10 @@ class Sound(commands.Cog):
             return await ctx.voice_client.move_to(voice_channel)
         await voice_channel.connect()
 
+    @commands.command(name="listsounds", help="Lists all available sounds")
+    async def listsounds(self, ctx):
+        await ctx.channel.send("Here's all of my very cool sounds I can play:\n```{a}```".format(a=", ".join(ListSoundFiles())))
+
     @commands.command(name="play", help="Plays a sound clip from the sounds folder")
     async def play(self, ctx, args):
         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("data/sounds/{}.mp3".format(args)))
@@ -24,3 +28,8 @@ class Sound(commands.Cog):
     @commands.command(name="stahp", help="Stops and disconnects the bot from voice")
     async def stahp(self, ctx):
         await ctx.voice_client.disconnect()
+
+def ListSoundFiles():
+    file_list=os.listdir("data/sounds")
+    fixed_list=[x.split('.')[0] for x in file_list]
+    return fixed_list

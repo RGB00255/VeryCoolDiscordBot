8cc24470d7a6e1f163fed76dd256abc05dc923b3
RGB00255 <ryan.bergeron1998@gmail.com>
2020-05-11 14:43:12 -0400

    Added + support to roll command, did some other things too

--- dnd.py
+++ dnd.py
@@ -8,7 +8,7 @@ class DnD(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="roll", help="Roll XdY")
+    @commands.command(name="roll", help="Roll XdY[+Z]")
     async def roll(self, ctx, args):
         await ctx.channel.send("{c.message.author.mention}, here's your roll: {r}".format(c=ctx, r=dRoll(args)))
 
@@ -18,19 +18,28 @@ def Roll(die):
 
 # Does a roll given a value like 3d4
 def dRoll(roll):
-    rollList = roll.split("d")
+    #rollList = roll.split("d")
+    rollList = re.split(r'[d+]+', roll)
     returnString = ""
     total = 0
 
     if rollList[0] == "1":
-        returnString += str(Roll(int(rollList[1])))
+        total = Roll(int(rollList[1]))
+        returnString += str(total)
     elif int(rollList[0]) > 0:
         for i in range(1, int(rollList[0]) + 1):
             tempRoll = Roll(int(rollList[1]))
             total += tempRoll
 
-            if i == int(rollList[0]): # If I is on its last run, finialize returnString
+            if i == int(rollList[0]): # If i is on its last run, finialize returnString
                 returnString += str(tempRoll) + " = " + str(total)
             else: # What normally executes
                 returnString += str(tempRoll) + " + "
-    return returnString
+    if len(rollList) == 3: # If + was in string
+        total += int(rollList[2])
+        return returnString + "\n+ {s} = {t}, very cool!".format(s=rollList[2], t=total)
+    if total == 1: # New return string if total sucks
+        return returnString + ", very cool?"
+    if total >= int(rollList[1]): # If rolled highest possible
+        return returnString + ", very very cool!"
+    return returnString + ", very cool!"
